54a2419b399f328c5170d46c86722b62d95757be
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-01 00:46:14 +0530

    Add : Initial Position

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public const int START_POS = 0;
+        static void Main(string[] args)
+        {
+            int currentPos = START_POS;
+        }
+    }
+}
