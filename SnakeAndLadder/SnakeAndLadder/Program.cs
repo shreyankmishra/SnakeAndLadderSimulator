8ccb84aee7221181b527bc6cafbbc198fe631de5
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-01 01:04:06 +0530

    Add : Rolling the die

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -5,9 +5,16 @@ namespace SnakeAndLadder
     class Program
     {
         public const int START_POS = 0;
+        public static int rollDie()
+        {
+            Random random = new Random();
+            int numDie = random.Next(1, 7);
+            return numDie;
+        }
         static void Main(string[] args)
         {
             int currentPos = START_POS;
+            int numberDie = rollDie();
         }
     }
 }
