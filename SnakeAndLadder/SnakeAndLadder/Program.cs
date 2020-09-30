e57af1cf3394f5b4b3b07bc8b2eba9884825b238
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-01 01:27:20 +0530

    Add : Checking Options for play

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -5,16 +5,40 @@ namespace SnakeAndLadder
     class Program
     {
         public const int START_POS = 0;
+        public const int END_POS = 100;
+        public const int NO_PLAY = 0;
+        public const int SNAKE = 1;
+        public const int LADDER = 2;
         public static int rollDie()
         {
             Random random = new Random();
             int numDie = random.Next(1, 7);
             return numDie;
         }
+
+        public static int newPos(int numOnDie)
+        {
+            int stepChange = 0;
+            Random random = new Random();
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case NO_PLAY:
+                    break;
+                case SNAKE:
+                    stepChange = -numOnDie;
+                    break;
+                case LADDER:
+                    stepChange = numOnDie;
+                    break;
+            }
+            return stepChange;
+        }
         static void Main(string[] args)
         {
             int currentPos = START_POS;
             int numberDie = rollDie();
+            int stepChange = newPos(numberDie);
         }
     }
 }
